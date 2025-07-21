6c7b972e2d7c04a18bc24577c162ac661c6ad805
Naru Ramesh Reddy <narurameshreddy@gmail.com>
2025-07-21 21:47:23 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,5 @@
 package com.example.demo;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @RestController
